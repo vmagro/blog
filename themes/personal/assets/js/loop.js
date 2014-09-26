3f2f5de843a2fed9a850a594208bad63901ec285
Vinnie Magro <vmagro@socaldevs.com>
2014-09-26 14:37:09 -0700

    Personalize hero unit text

--- themes/personal/assets/js/loop.js
+++ themes/personal/assets/js/loop.js
@@ -84,9 +84,9 @@ jQuery( function($){
 			slide_links: 'blank',		// Individual links for each slide (Options: false, 'num', 'name', 'blank')
 			thumb_links: 0,				// Individual thumb links for each slide
 			slides:  	[				// Slideshow Images
-							{image : 'assets/img/sliders/slider1.jpg', title : '<div class="hero-text"><h2 class="hero-heading">HANDCRAFTED</h2><p>Built to provide great visitor experience</p></div>', thumb : '', url : ''},
-							{image : 'assets/img/sliders/slider2.jpg', title : '<div class="hero-text"><h2 class="hero-heading">PARALLAX</h2><p>Scrolling the page is fun with parallax background</p></div>', thumb : '', url : ''},
-							{image : 'assets/img/sliders/slider3.jpg', title : '<div class="hero-text"><h2 class="hero-heading">BUY ONE FOR TWO</h2><p>Buy one to get both of the agency and personal theme</p></div>', thumb : '', url : ''}
+							{image : 'assets/img/sliders/slider1.jpg', title : '<div class="hero-text"><h2 class="hero-heading">VINNIE MAGRO</h2><p>Hacker</p></div>', thumb : '', url : ''},
+							{image : 'assets/img/sliders/slider2.jpg', title : '<div class="hero-text"><h2 class="hero-heading">VINNIE MAGRO</h2><p>Computer Engineer</p></div>', thumb : '', url : ''},
+							{image : 'assets/img/sliders/slider3.jpg', title : '<div class="hero-text"><h2 class="hero-heading">VINNIE MAGRO</h2><p>Tech Enthusiast</p></div>', thumb : '', url : ''}
 						],
 		});
 
